dfde940fa2c32fbd3da8584328f2cea73d326290
mohitkrsuman <mohit.kumar@sellogs.com>
2025-07-28 22:30:59 +0530

    tailwind with custoer variables done

--- client/tailwind.config.mjs
+++ client/tailwind.config.mjs
@@ -1,19 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        'primary-color': 'var(--primary-color)',
-        'secondary-color': 'var(--secondary-color)',
-        'accent-color': 'var(--accent-color)',
+        'primary': 'var(--primary-color)',
+        'secondary': 'var(--secondary-color)',
+        'accent': 'var(--accent-color)',
+        'text-primary': 'var(--text-primary)',
+        'background-light': 'var(--background-light)',
       },
     },
   },
   plugins: [],
-}
+};
