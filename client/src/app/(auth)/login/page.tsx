318ab8ef58894cb9aa1709e9f590ca864d28672e
mohitkrsuman <mohit.kumar@sellogs.com>
2025-07-28 21:44:21 +0530

    project structure added

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>login page</div>
+    <div className=''>login page</div>
   )
 }
 
---
+++ client/src/app/dashboard/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashPageById = () => {
+   return (
+      <div>DashPageById</div>
+   )
+}
+
+export default DashPageById
