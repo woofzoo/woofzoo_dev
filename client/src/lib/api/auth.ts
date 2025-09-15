af98e12ff3c7d486a967b410dd52b049011cbc82
mohitkrsuman <mohit.kumar@sellogs.com>
2025-09-15 20:40:45 +0530

    save tokens

--- client/src/lib/api/auth.ts
+++ client/src/lib/api/auth.ts
@@ -2,17 +2,17 @@ import api from "../axios";
 import { LoginPayload } from "@/types/auth";
 
 export const setTokens = (access: string, refresh: string) => {
-   localStorage.setItem("token", access);
-   localStorage.setItem("refreshToken", refresh);
-}
+  localStorage.setItem("access_token", access);
+  localStorage.setItem("refresh_token", refresh);
+};
 
-export const getAccessToken = () => localStorage.getItem("token");
+export const getAccessToken = () => localStorage.getItem("accesstoken");
 export const getRefreshToken = () => localStorage.getItem("refresh_token");
 
 export const clearTokens = () => {
-   localStorage.removeItem("access_token");
-   localStorage.removeItem("refresh_token");
-}
+  localStorage.removeItem("access_token");
+  localStorage.removeItem("refresh_token");
+};
 
 export const login = async (payload: LoginPayload) => {
   const { data } = await api.post("/auth/login", payload);
